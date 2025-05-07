f06ed3f3279c3397d537a7f66b8595ca51fcb22f
lazlanrafar <lazlanrafar@gmail.com>
2025-05-07 15:53:43 +0800

    update favicon

--- app.vue
+++ app.vue
@@ -1,58 +1,59 @@
 <script setup lang="ts">
-import { Sonner } from '@/components/ui/sonner'
-import { ConfigProvider } from 'radix-vue'
+import { Sonner } from "@/components/ui/sonner";
+import { ConfigProvider } from "radix-vue";
 
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
-const color = computed(() => colorMode.value === 'dark' ? '#09090b' : '#ffffff')
+const color = computed(() =>
+  colorMode.value === "dark" ? "#09090b" : "#ffffff"
+);
 
-const { theme, radius } = useCustomize()
+const { theme, radius } = useCustomize();
 
 useHead({
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color },
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
   ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' },
-  ],
+  link: [{ rel: "icon", href: "/favicon.ico" }],
   htmlAttrs: {
-    lang: 'en',
+    lang: "en",
   },
   bodyAttrs: {
     class: computed(() => `theme-${theme.value}`),
     style: computed(() => `--radius: ${radius.value}rem;`),
   },
-})
+});
 
-const title = 'Nuxt Shadcn UI - Dashboard Template'
-const description = 'This dashboard, built with Nuxt, Shadcn UI, and UnoCSS. It includes a dark mode toggle and is optimized for performance and data efficiency.'
+const title = "KadoCMS - Headless CMS";
+const description =
+  "Headless CMS for your next project. KadoCMS is a headless CMS that allows you to create and manage your content easily. It is built with Vue 3, Nuxt 3, and Tailwind CSS.";
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogUrl: 'https://dashboard.dianprata.com',
-  ogImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
+  ogUrl: "https://dashboard.dianprata.com",
+  ogImage: "https://nuxt-shadcn-dashboard.vercel.app/social-card.png",
   twitterTitle: title,
   twitterDescription: description,
-  twitterImage: 'https://nuxt-shadcn-dashboard.vercel.app/social-card.png',
-  twitterCard: 'summary_large_image',
-})
+  twitterImage: "https://nuxt-shadcn-dashboard.vercel.app/social-card.png",
+  twitterCard: "summary_large_image",
+});
 
-const router = useRouter()
+const router = useRouter();
 
 defineShortcuts({
-  'G-H': () => router.push('/'),
-  'G-E': () => router.push('/email'),
-})
+  "G-H": () => router.push("/"),
+  "G-E": () => router.push("/email"),
+});
 
-const useIdFunction = () => useId()
+const useIdFunction = () => useId();
 
-const textDirection = useTextDirection({ initialValue: 'ltr' })
-const dir = computed(() => textDirection.value === 'rtl' ? 'rtl' : 'ltr')
+const textDirection = useTextDirection({ initialValue: "ltr" });
+const dir = computed(() => (textDirection.value === "rtl" ? "rtl" : "ltr"));
 </script>
 
 <template>
